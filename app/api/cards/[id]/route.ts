eacdcf6fc894f760eca5792bd295361014d7ccf4
Gabriel Nunes <gnmf2000@gmail.com>
2026-01-03 16:28:17 -0300

    chore: updating prisma

--- app/api/cards/[id]/route.ts
+++ app/api/cards/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { prisma } from '@/lib/prisma'
+import prisma from '@/lib/prisma'
 import { FlashcardUpdateInput } from '@/lib/types'
 
 export async function GET(
